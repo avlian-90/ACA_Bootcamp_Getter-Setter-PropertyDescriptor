173331249482abeed8d2639a830d8bbacc8fc294
Liana <lianavagyan.90@gmail.com>
2021-12-23 04:33:18 +0400

    feat: getter and setter are completed and added

---
+++ Task1.js
@@ -0,0 +1,41 @@
+/* - Write an object with field name.
+
+```javascript
+  const obj = {
+    name: [], // ['name', length][]
+    set name,
+    get name,
+  }
+
+  console.log(obj.name)
+
+  obj.name = 'Hovhannes, Sona';
+
+  console.log(obj.name) // [['Hovhannes', 9], ['Sona', 4]]
+``` */
+
+const obj = {
+    _name: [],
+
+    set name(value) {
+        let [hovo, sona] = value.split(", ");
+        if (hovo.length > 0 && sona.length > 0) {
+            obj._name[0] = [hovo, hovo.length];
+            obj._name[1] = [sona, sona.length];
+        } else {
+            return "Not a valid name";
+        }  
+    },
+
+    get name() {
+        return obj._name;
+    }
+}
+
+console.log(obj._name)
+
+obj.name = 'Hovhannes, Sona';
+
+console.log(obj._name) // [['Hovhannes', 9], ['Sona', 4]]
+
+
